ad2392266b87a9d18f5aea45c1c10e06de6f2819
tsayjunior <juniorllanos15@gmail.com>
2023-10-30 01:07:48 -0400

    change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>probando appsadasds</Text>
+      <Text>probando app</Text>
       <StatusBar style="auto" />
     </View>
   );
